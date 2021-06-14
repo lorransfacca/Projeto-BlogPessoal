e2a575677c3e7498fc507bca77a8567961f84093
Lorrans Facca <59665341+lorransfacca@users.noreply.github.com>
2021-06-14 16:04:48 -0300

    Update PostagemRepository.java

--- src/main/java/com/generation/blogPessoalLorrans/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogPessoalLorrans/repository/PostagemRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.generation.blogPessoalMatheus.model.Postagem;
+import com.generation.blogPessoalLorrans.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {	
